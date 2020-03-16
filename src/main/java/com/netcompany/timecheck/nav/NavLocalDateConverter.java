bcec959282dfa6f4db25cf1379fe14f04a8b9a91
Torbjørn Skyberg Knutsen <torbjornsk@gmail.com>
2020-03-16 13:36:24 +0100

    Benytter riktig datoformat for nav

--- src/main/java/com/netcompany/timecheck/nav/NavLocalDateConverter.java
+++ src/main/java/com/netcompany/timecheck/nav/NavLocalDateConverter.java
@@ -8,7 +8,7 @@ import java.time.format.DateTimeFormatter;
 public class NavLocalDateConverter extends AbstractBeanField {
     @Override
     protected Object convert(final String s) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         return LocalDate.parse(s, formatter);
     }
 }
